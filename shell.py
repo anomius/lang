3f9da5a78d904f4217046d7ce963a4e0291843d5
anomius <swarajpurohit@gmail.com>
2021-01-22 10:51:05 +0530

    -- finalay update lexer

--- shell.py
+++ shell.py
@@ -1,9 +1,8 @@
 import basic
 
 while True:
-    text=input("Basic >>")
-    results,error =basic.run(text)
-    
-    if error = print(error.as_string())
-    else: print(results)
-    print(text)
+    text = input('Basic >> ')
+    result, error = basic.run('<stdin>', text)
+
+    if error: print(error)
+    else: print(result)
